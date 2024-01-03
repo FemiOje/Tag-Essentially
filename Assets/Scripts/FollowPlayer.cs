863791deea64b91d8a8bff7e2e20d307eab6d36c
Femi Ojetokun <ojetokunakinwale@gmail.com>
2024-01-03 14:52:22 +0100

    add: AutoLevel asset for level generation

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,22 +4,4 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    [SerializeField] private Transform playerPos;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position += (playerPos.position - transform.position) * 1.0f * Time.deltaTime;
-        transform.position = new Vector3(transform.position.x, 1, transform.position.z);
-
-        if (transform.position == playerPos.position)
-        {
-            transform.position += Vector3.zero;
-        }
-    }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1.0f;
+    }
+}
