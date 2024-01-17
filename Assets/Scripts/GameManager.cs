7e5932373f1e742a6ce3ff4d073bddb34d575224
Femi Ojetokun <ojetokunakinwale@gmail.com>
2024-01-17 15:09:28 +0100

    simple game over check

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,23 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public bool isGameOver;
+    public Player player;
+
+
+    private void Start()
+    {
+        isGameOver = false;
+    }
+
+    private void Update()
+    {
+        if (player.health <= 0)
+        {
+            PlayGameOverSequence();
+        }
+    }
+
     public void PauseGame()
     {
         Time.timeScale = 0f;
@@ -15,4 +32,9 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 1.0f;
     }
 
+    public void PlayGameOverSequence()
+    {
+        isGameOver = true;
+        Debug.Log("Game over");
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,8 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody _rigidbody;
     [SerializeField] public int health;
     [SerializeField] public int speed;
     [SerializeField] public int damagePoints;
-
-    private void Awake() {
-        _rigidbody = GetComponent<Rigidbody>();
-    }
 }
